7e784c270ad63670631a9efdad69f0a7cd4e3323
Nikhil Kumar <nikhilkumarsingh166@gmail.com>
2022-10-02 12:59:27 +0530

    input taken

--- app.js
+++ app.js
@@ -2,14 +2,15 @@
 var btnTranslate = document.querySelector("#btn-translate"); //reference to the button
 var textInput = document.querySelector("#text-input");
 
-console.log(textInput);
-console.log("input", textInput.value);
+
 
 
 function clickEventHandler(){
     console.log("Button clicked!!!");  //adding function to the button using click listener
+    // console.log(textInput);
+    console.log("input-->", textInput.value);
 };
 
-btnTranslate.addEventListener("click", clickEventHandler()); //calling back function
+btnTranslate.addEventListener("click", clickEventHandler); //calling back function
 
 
