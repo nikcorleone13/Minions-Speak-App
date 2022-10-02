76573b6762d25a9ef1713fe23fa9d0a491381df9
Nikhil Kumar <nikhilkumarsingh166@gmail.com>
2022-10-02 13:16:49 +0530

    output done

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // console.log("Hello form app.js");
 var btnTranslate = document.querySelector("#btn-translate"); //reference to the button
 var textInput = document.querySelector("#text-input");
-
+var outputDiv  = document.querySelector("#output");
 
 
 
 function clickEventHandler(){
-    console.log("Button clicked!!!");  //adding function to the button using click listener
-    // console.log(textInput);
-    console.log("input-->", textInput.value);
+    outputDiv.innerText = "asasasasasasas" + textInput.value;
+
+    // console.log("Button clicked!!!");  //adding function to the button using click listener
+    // // console.log(textInput);
+    // console.log("input-->", textInput.value);
 };
 
 btnTranslate.addEventListener("click", clickEventHandler); //calling back function
